bcd6db5a30ec84948ab22b2c6f563c95e308f461
shrawnz <shreyansm97@gmail.com>
2017-08-03 12:03:54 +0530

    hashtah highchart

--- tweet.py
+++ tweet.py
@@ -53,34 +53,8 @@ class TweetModel(db.Document):
 
 @app.route("/")
 def index():
-	# all_tweets = tweepy_api.user_timeline('shrawnz23')
-	all_tweets = tweepy_api.search('modi OR bjp pm OR pm modi')
-	tweets=[]
-	# print (all_tweets[0], file=sys.stderr)
-	for tweet in all_tweets:
-		print(tweet.retweeted,file=sys.stderr)
-		username = tweet.user.name
-		content = tweet.text
-		retweeted = tweet.retweeted
-		hashtags = [h['text'] for h in tweet.entities['hashtags']]
-		location = tweet.user.location
-		favourite = tweet.favorite_count
-		coordinates=tweet.coordinates
-		tweetFor = TYPE_MODI
-		url_id = str(tweet.id)
-		tweetType = TWEETTYPE_TEXT
-		if('media' in tweet.entities):
-			if content:
-				tweetType = TWEETTYPE_TEXTIMG
-			else:
-				tweetType = TWEETTYPE_IMAGE
-
-		model = TweetModel(user=username,content=content,retweeted=retweeted,hashtags=hashtags,location=location,favourite=favourite,\
-				coordinates=coordinates,tweetType=tweetType ,tweetFor=tweetFor,url_id=url_id)
-		model.save()
-		tweets.append([tweet.id,username,content,hashtags,retweeted,location,favourite,coordinates,tweetType])
-
-	return render_template('index.html', tweets = tweets)
+	all_tweets = tweepy_api.user_timeline('shrawnz23')
+	return("Hello!")
 
 @app.route("/showType")
 def showType():
@@ -103,29 +77,103 @@ def showType():
 
 @app.route("/showHashtag")
 def showHashtag():
-	tweets = TweetModel.query.all()
+	tweets = TweetModel.query.filter(TweetModel.tweetFor==TYPE_MODI).all()
 	hash_dict = dict()
 	for tweet in tweets:
 		tags = tweet.hashtags
 		for t in tags:
 			hash_dict[t] = hash_dict.get(t, 0) + 1
-	result = sorted(hash_dict.items(), key=lambda x: x[1],reverse=True)
+	result_m = sorted(hash_dict.items(), key=lambda x: x[1],reverse=True)
+
+	tweets = TweetModel.query.filter(TweetModel.tweetFor==TYPE_KEJRI).all()
+	hash_dict = dict()
+	for tweet in tweets:
+		tags = tweet.hashtags
+		for t in tags:
+			hash_dict[t] = hash_dict.get(t, 0) + 1
+	result_k = sorted(hash_dict.items(), key=lambda x: x[1],reverse=True)
 	# print (result,file=sys.stderr)
-	return render_template('hashtag.html',tags=result[:10])
+	return render_template('hashtag.html',tags_m=result_m[:10],tags_k=result_k[:10])
 
 @app.route("/showFav")
 def showFav():
 	original_tweets = TweetModel.query.filter(TweetModel.retweeted==False).all()
-	for tweet in original:
-		
+	# for tweet in original:
+	return("ok")	
+
+@app.route("/getData")
+def getData():
+	limit = tweepy_api.rate_limit_status()
+	cnt = limit['resources']['search']['/search/tweets']['limit']
+	while(cnt):
+		resp = getTweetM()
+
+	return("Done")
 
 @app.route("/getModiData")
 def getTweetM():
-	return("Modi data stored!")
+	all_tweets = tweepy_api.search('modi OR bjp pm OR pm modi')
+	tweets=[]
+	for tweet in all_tweets:
+		username = tweet.user.name
+		content = tweet.text
+		fword = content.split(" ")[0]
+		retweeted = False
+		if fword=='RT':
+			retweeted = True
+		print (retweeted, file=sys.stderr)
+		hashtags = [h['text'] for h in tweet.entities['hashtags']]
+		location = tweet.user.location
+		favourite = tweet.favorite_count
+		coordinates=tweet.coordinates
+		tweetFor = TYPE_MODI
+		url_id = str(tweet.id)
+		tweetType = TWEETTYPE_TEXT
+		if('media' in tweet.entities):
+			if content:
+				tweetType = TWEETTYPE_TEXTIMG
+			else:
+				tweetType = TWEETTYPE_IMAGE
+
+		model = TweetModel(user=username,content=content,retweeted=retweeted,hashtags=hashtags,location=location,favourite=favourite,\
+				coordinates=coordinates,tweetType=tweetType ,tweetFor=tweetFor,url_id=url_id)
+		model.save()
+		tweets.append([tweet.id,username,content,hashtags,retweeted,location,favourite,coordinates,tweetType])
+
+	# return(1)
+	return render_template('index.html', tweets = tweets)
 
 @app.route("/getKejriData")
 def getTweetK():
-	return ("Kejri data stored")
+	all_tweets = tweepy_api.search('kejriwal OR aam aadmi party OR aap delhi')
+	tweets=[]
+	# print (all_tweets[0], file=sys.stderr)
+	for tweet in all_tweets:
+		username = tweet.user.name
+		content = tweet.text
+		fword = content.split(" ")[0]
+		retweeted = False
+		if fword=='RT':
+			retweeted = True
+		hashtags = [h['text'] for h in tweet.entities['hashtags']]
+		location = tweet.user.location
+		favourite = tweet.favorite_count
+		coordinates=tweet.coordinates
+		tweetFor = TYPE_KEJRI
+		url_id = str(tweet.id)
+		tweetType = TWEETTYPE_TEXT
+		if('media' in tweet.entities):
+			if content:
+				tweetType = TWEETTYPE_TEXTIMG
+			else:
+				tweetType = TWEETTYPE_IMAGE
+
+		model = TweetModel(user=username,content=content,retweeted=retweeted,hashtags=hashtags,location=location,favourite=favourite,\
+				coordinates=coordinates,tweetType=tweetType ,tweetFor=tweetFor,url_id=url_id)
+		model.save()
+		tweets.append([tweet.id,username,content,hashtags,retweeted,location,favourite,coordinates,tweetType])
+
+	return render_template('index.html', tweets = tweets)
 
 if __name__ == "__main__":
 	app.run()
